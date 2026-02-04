6622213f34a6097ebde47b2023eac6dd8625ec52
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-02-04 14:50:43 +0530

    index page added

--- CourseKartapp/urls.py
+++ CourseKartapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-
+from .views import index,courses,about,contact
 urlpatterns = [
-    
+   path('',index, name="index"),
+   path('courses/',courses, name="courses"),
+   path('about/',about, name="about"),
+   path('contact/',contact, name="contact"),
 ]
--- CourseKartapp/views.py
+++ CourseKartapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'CourseKartapp/index.html')
+
+def courses(request):
+    return render(request, 'CourseKartapp/courses.html')
+
+def about(request):
+    return render(request, 'CourseKartapp/about.html')
+def contact(request):
+    return render(request, 'CourseKartapp/contact.html')
